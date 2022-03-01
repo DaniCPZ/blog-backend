c2cf90546f917901fde19821bec28b957ebc9004
Daniel Valenzuela <danielvalenzuela199825@gmail.com>
2022-03-01 15:00:15 -0300

    Refactor the image upload to it's own class

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Inertia\Inertia;
 use App\Models\Setting;
+use App\Actions\UploadFile;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -37,18 +38,16 @@ class SettingsController extends Controller
         if ($request->file('hero_image')) {
             $this->settings->deleteHeroImage();
 
-            $imageName = (string) Str::of($request->file('hero_image')->getClientOriginalName())
-                ->beforeLast('.')
-                ->slug()
-                ->append('.')
-                ->append($request->file('hero_image')->getClientOriginalExtension());
+            $imageName = (new UploadFile)
+                ->setFile($request->file('hero_image'))
+                ->setUploadPath($this->settings->uploadFolder())
+                ->execute();
 
             $data['hero_image'] = $imageName;
-
-            $request->file('hero_image')->storeAs($this->settings->uploadFolder(), $imageName, 'public');
         }
+        $mergedData = array_merge($this->settings->data, $data);
 
-        $this->settings->data = $data;
+        $this->settings->data = $mergedData;
         $this->settings->save();
 
         return redirect()->back();
